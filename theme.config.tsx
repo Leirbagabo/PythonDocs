6e6344b5b20cb6e832f71b01615a5103d0b4a775
Gabriel <leirbagaboo@gmail.com>
2024-11-17 17:25:34 -0400

    estructura lista, contenido de prerequisitos lista

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Python</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Leirbagabo/pythondocs',
   },
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Todos los derechos reservados, los zurdos no jaja. Ajá me reservo el derecho de agregar y quitar a voluntad y dar de baja el sitio en el momento que considere necesario',
   },
 }
 
